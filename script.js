7b98cd2b32441c32e05699e34d80c1f783e540b3
BiwashGurung <varungrg10@gmail.com>
2023-08-12 16:53:59 +0545

    feat : decleared valiable

---
+++ script.js
@@ -0,0 +1,16 @@
+const taskInput = document.querySelector(".task-input input"),
+filters = document.querySelectorAll(".filters span"),
+clearAll = document.querySelector(".clear-btn"),
+taskBox = document.querySelector(".task-box");
+
+let editId,
+isEditTask = false,
+todos = JSON.parse(localStorage.getItem("todo-list"));
+
+filters.forEach(btn => {
+    btn.addEventListener("click", () => {
+        document.querySelector("span.active").classList.remove("active");
+        btn.classList.add("active");
+        showTodo(btn.id);
+    });
+});
