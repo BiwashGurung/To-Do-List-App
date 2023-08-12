25cbb1657d893d03d7c0ec4e78f7423e1d831ea2
BiwashGurung <varungrg10@gmail.com>
2023-08-12 16:59:15 +0545

    feat : done with editTask Function

--- script.js
+++ script.js
@@ -62,3 +62,10 @@ function updateStatus(selectedTask) {
     }
     localStorage.setItem("todo-list", JSON.stringify(todos))
 }
+function editTask(taskId, textName) {
+    editId = taskId;
+    isEditTask = true;
+    taskInput.value = textName;
+    taskInput.focus();
+    taskInput.classList.add("active");
+}
